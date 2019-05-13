8dc657535f154abe1ff6e8ac1c232b6e698c405d
Šimon Zouvala <445475@mail.muni.cz>
2019-05-13 17:50:25 +0200

    all buttons set

--- hotel-gui/src/main/java/cz/muni/fi/pv168/hotel/gui/MainWindow.java
+++ hotel-gui/src/main/java/cz/muni/fi/pv168/hotel/gui/MainWindow.java
@@ -7,6 +7,7 @@ package cz.muni.fi.pv168.hotel.gui;
 
 import cz.muni.fi.pv168.hotel.GuestManager;
 import cz.muni.fi.pv168.hotel.RoomManager;
+
 import javax.swing.ListModel;
 
 /**
@@ -23,6 +24,7 @@ public class MainWindow extends javax.swing.JFrame {
         this.roomManager = roomManager;
         this.guestManager = guestManager;
         initComponents();
+        
     }
 
     /**
@@ -38,12 +40,21 @@ public class MainWindow extends javax.swing.JFrame {
         roomButton = new javax.swing.JButton();
         guestButton = new javax.swing.JButton();
         checkoutButton = new javax.swing.JButton();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        selectionTabel = new javax.swing.JList<>();
         jScrollPane1 = new javax.swing.JScrollPane();
         showTextArea = new javax.swing.JTextArea();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        selectionTabel = new javax.swing.JList<>();
+        addRoom = new javax.swing.JButton();
+        removeRoom = new javax.swing.JButton();
+        findTextField = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowActivated(java.awt.event.WindowEvent evt) {
+                formWindowActivated(evt);
+            }
+        });
 
         accommodation.setText("Ubytovat");
         accommodation.setToolTipText("");
@@ -54,11 +65,6 @@ public class MainWindow extends javax.swing.JFrame {
         });
 
         roomButton.setText("Pokoje");
-        roomButton.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                roomButtonMouseClicked(evt);
-            }
-        });
         roomButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 roomButtonActionPerformed(evt);
@@ -79,6 +85,10 @@ public class MainWindow extends javax.swing.JFrame {
 
         checkoutButton.setText("Odhlasit hosta");
 
+        showTextArea.setColumns(20);
+        showTextArea.setRows(5);
+        jScrollPane1.setViewportView(showTextArea);
+
         selectionTabel.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 selectionTabelMouseClicked(evt);
@@ -86,9 +96,21 @@ public class MainWindow extends javax.swing.JFrame {
         });
         jScrollPane2.setViewportView(selectionTabel);
 
-        showTextArea.setColumns(20);
-        showTextArea.setRows(5);
-        jScrollPane1.setViewportView(showTextArea);
+        jScrollPane3.setViewportView(jScrollPane2);
+
+        addRoom.setText("+");
+        addRoom.setFocusable(false);
+        addRoom.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                addRoomActionPerformed(evt);
+            }
+        });
+
+        removeRoom.setText("-");
+        removeRoom.setFocusable(false);
+
+        findTextField.setText("Hledej...");
+        findTextField.setToolTipText("Hledej...");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -103,18 +125,25 @@ public class MainWindow extends javax.swing.JFrame {
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(roomButton)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(guestButton)))
-                        .addGap(187, 187, 187))
-                    .addComponent(jScrollPane2))
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(guestButton))
+                            .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 351, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(18, 181, Short.MAX_VALUE)
+                                .addComponent(checkoutButton))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(8, 8, 8)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jScrollPane1)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(findTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 236, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(0, 0, Short.MAX_VALUE))))))
                     .addGroup(layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 182, Short.MAX_VALUE)
-                        .addComponent(checkoutButton)
-                        .addGap(24, 24, 24))
-                    .addGroup(layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 314, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                        .addComponent(addRoom, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(removeRoom, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -122,18 +151,23 @@ public class MainWindow extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(accommodation, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(accommodation, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(roomButton)
                             .addComponent(guestButton))
-                        .addGap(24, 24, 24))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(checkoutButton, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(61, 61, 61)))
+                        .addGap(85, 85, 85)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(addRoom)
+                    .addComponent(removeRoom, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(findTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 246, Short.MAX_VALUE)
-                    .addComponent(jScrollPane1))
+                    .addComponent(jScrollPane1)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 246, Short.MAX_VALUE))
                 .addContainerGap())
         );
 
@@ -146,23 +180,21 @@ public class MainWindow extends javax.swing.JFrame {
 
     private void guestButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_guestButtonActionPerformed
         // TODO add your handling code here:
+         selectionTabel.setModel((ListModel<String>) new GuestListModel(guestManager.findAllGuest()));
+        findTextField.setVisible(true);
+        addRoom.setVisible(false);
+        removeRoom.setVisible(false);
+        
     }//GEN-LAST:event_guestButtonActionPerformed
 
     private void roomButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_roomButtonActionPerformed
         // TODO add your handling code here:
+          selectionTabel.setModel((ListModel<String>) new RoomListModel(roomManager.findAllRooms()));
+        addRoom.setVisible(true);
+        findTextField.setVisible(false);
         
                 
     }//GEN-LAST:event_roomButtonActionPerformed
-
-    private void roomButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_roomButtonMouseClicked
-        // TODO add your handling code here:
-        selectionTabel.setModel((ListModel<String>) new RoomListModel(roomManager.findAllRooms()));
-    }//GEN-LAST:event_roomButtonMouseClicked
-
-    private void guestButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_guestButtonMouseClicked
-        // TODO add your handling code here:
-        selectionTabel.setModel((ListModel<String>) new GuestListModel(guestManager.findAllGuest()));
-    }//GEN-LAST:event_guestButtonMouseClicked
 
     private void selectionTabelMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_selectionTabelMouseClicked
         // TODO add your handling code here:
@@ -174,7 +206,7 @@ public class MainWindow extends javax.swing.JFrame {
             showTextArea.append( "Číslo pokoje: " + roomManager.findAllRooms().get(indexOfValue).getNumber()+ "\n");
             showTextArea.append( "Cena: " + roomManager.findAllRooms().get(indexOfValue).getPrice()+ "\n");
             showTextArea.append( "Kapacita: " + roomManager.findAllRooms().get(indexOfValue).getCapacity()+ "\n");
-            
+            removeRoom.setVisible(true);
         }
         if ((selectionTabel.getModel() instanceof GuestListModel)) {
             showTextArea.append("ID: " + guestManager.findAllGuest().get(indexOfValue).getId() + "\n");
@@ -190,14 +222,35 @@ public class MainWindow extends javax.swing.JFrame {
         
     }//GEN-LAST:event_selectionTabelMouseClicked
 
+    private void addRoomActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addRoomActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_addRoomActionPerformed
+
+    private void guestButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_guestButtonMouseClicked
+        // TODO add your handling code here:
+
+    }//GEN-LAST:event_guestButtonMouseClicked
+
+    private void formWindowActivated(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowActivated
+        // TODO add your handling code here:
+        addRoom.setVisible(false);
+        removeRoom.setVisible(false);
+        findTextField.setVisible(false);
+    }//GEN-LAST:event_formWindowActivated
+
  
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton accommodation;
+    private javax.swing.JButton addRoom;
     private javax.swing.JButton checkoutButton;
+    private javax.swing.JTextField findTextField;
     private javax.swing.JButton guestButton;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JButton removeRoom;
     private javax.swing.JButton roomButton;
     private javax.swing.JList<String> selectionTabel;
     private javax.swing.JTextArea showTextArea;
